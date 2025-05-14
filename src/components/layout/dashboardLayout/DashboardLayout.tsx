ad70df1dee8762df01436faa7ff83f15bccdad80
trev <trevorjacobsykes@gmail.com>
2025-05-14 15:54:07 -0700

    init

--- src/components/layout/dashboardLayout/DashboardLayout.tsx
+++ src/components/layout/dashboardLayout/DashboardLayout.tsx
@@ -1,27 +1,16 @@
 // DashboardLayout.jsx
-import { useAccount, useConnect } from 'wagmi'
 import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
-import { Profile } from '../../profile/Profile'
 import { ConnectWallet } from '../../wallet/ConnectWallet'
 import DashboardNav from '../../nav/DashboardNav'
 import styles from './DashboardLayout.module.css'
 
 const DashboardLayout = () => {
-    const { address } = useAccount()
-    const { status } = useConnect()
+
     const [isConnectOpen, setIsConnectOpen] = useState(false)
 
     return (
         <div className={styles.wrapper}>
-            <header className={styles.header}>
-                <Profile />
-                {!address && status !== 'success' && (
-                    <button className={styles.connectButton} onClick={() => setIsConnectOpen(true)}>
-                        Connect Wallet
-                    </button>
-                )}
-            </header>
 
             <main className={styles.main}>
                 <Outlet />
